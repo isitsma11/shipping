fb2ac72977caf54408202d2e592bba2b2eebca86
isitsma11 <112361320+isitsma11@users.noreply.github.com>
2023-12-02 11:26:08 +0500

    Update delete_barang.php

--- delete_barang.php
+++ delete_barang.php
@@ -1,12 +1,8 @@
 <?php
-// include database connection file
-include_once("connection.php");
+    require "koneksi.php";
 
-// Get id from URL to delete that user
-$id_barang = $_GET['id_barang'];
+    $admin_karyawan = $_GET['admin_karyawan'];
+    $result = mysqli_query($conn, "DELETE FROM shipping WHERE admin_karyawan=$id_karyawan");
 
-// Delete user row from table based on given id
-$result = mysqli_query($conn, "DELETE FROM tbl_barang WHERE id_barang='$id_barang'");
-
-// After delete redirect to Home, so that latest user list will be displayed.
-header("Location:index.php");
+    header("Location: index.php");
+?>
